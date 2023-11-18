4827d3f603f8fe990954d50b3849842af894c7b2
Emediong Bassey Edem <edememediong1@gmail.com>
2023-11-18 23:22:33 +0100

    Add the different dataset to the schema

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+
+
+
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+// Then we give our schema to the builder and provide the result to Sanity
+export const schemaTypes = [works, testimonials, brands, abouts, skills, workExperience, experiences, contact]
