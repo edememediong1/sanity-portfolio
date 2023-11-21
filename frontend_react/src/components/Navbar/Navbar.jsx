6890017c83b5514f4b0f845f598fb0661bad079b
Emediong Bassey Edem <edememediong1@gmail.com>
2023-11-21 22:42:24 +0100

    added navbar component

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import './Navbar.scss'
-import {images} from '../../constants'
+import {images} from '/src/constants'
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>
+    <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
             <img src={images.logo} alt="logo"/>
         </div>
+        <ul className='app__navbar-links'>
+            {["home", "about", "work", "skills", "contact" ].map((item) =>(
+                <li className="app__flex p-text" key={`link-${item}`}>
+                    <div/>
+                    <a href={`#${item}`}>{item}</a>
+                </li>
+            ))}
+        </ul>
     </nav>
   )
 }
